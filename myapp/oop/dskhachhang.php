0024890111a0ca22743e7926a933f43d2a5f341e
Kato-30 <trandinhan19112003@gmail.com>
2023-12-28 17:06:09 +0700

    cmt

--- myapp/oop/dskhachhang.php
+++ myapp/oop/dskhachhang.php
@@ -7,47 +7,40 @@
         <td>Tuoi</td>
         <td>Chuc nang</td>
     </tr>
-    <?php
+<?php    
     include("khachhang.php");
 
     // $kh = new KhachHang('kh09', 'Ănfwa', 20);
-    $action = isset($_GET["action"]) ? $_GET["action"] : "0";
+    $action = isset($_GET["action"]) ? $_GET["action"] :"0";
     //Xóa khách hàng nếu có yêu cầu
     if ($action == "2") {
-        $makhach = isset($_GET["makhach"]) ? $_GET["makhach"] : "";
-        if ($makhach != "") {
-            $success = HoSo::Delete($makhach);
-            if ($success) {
+        $makhach=isset($_GET["makhach"]) ? $_GET["makhach"] :"";
+        if($makhach != "") {
+            $success = KhachHang::Delete($makhach);
+            if($success){
                 echo "<script> alert('Delete success!');</script>";
-            } else {
+            }else{
                 echo "<script> alert('Delete failed!');</script>";
             }
-        }
+        }    
     }
     //Lấy danh sách khách hàng
-    $ds = HoSo::GetAll();
+    $ds = KhachHang::GetAll() ;
     foreach ($ds as $item) {
-        ?>
+?>
         <tr>
-            <td>
-                <?php echo $item->makhach ?>
-            </td>
-            <td>
-                <?php echo $item->tenkhach ?>
-            </td>
-            <td>
-                <?php echo $item->tuoi ?>
-            </td>
+            <td><?php echo $item->makhach ?></td>
+            <td><?php echo $item->tenkhach ?></td>
+            <td><?php echo $item->tuoi ?></td>
             <td>
                 <a href="dskhachhang.php?action=1&makhach=<?php echo $item->makhach ?>">Edit</a>
                 <span> | </span>
                 <a onclick="return confirm('Do you want to delete this customer?');"
-                    href="dskhachhang.php?action=2&makhach=<?php echo $item->makhach ?>">Delete</a>
+                 href="dskhachhang.php?action=2&makhach=<?php echo $item->makhach ?>">Delete</a>
             </td>
         </tr>
-        <?php
+<?php        
     }
-
-    echo "Đáaaaaa";
-    ?>
+    echo "aghjkl;";
+?>    
 </table>
